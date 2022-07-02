2c26d2653d1cf965a22757915fb4511879fa4805
Patchar <pchrp_pt@hotmail.com>
2022-07-02 14:31:59 +0700

    Initial Commit

---
+++ TechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Patchar Promtha on 2/7/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
